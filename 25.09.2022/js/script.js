1ecad167f2643a31d177eba71d8fde2124447bbf
emik25 <115448156+emik25@users.noreply.github.com>
2022-10-11 22:33:29 +0500

    Delete 25.09.2022 directory

--- 25.09.2022/js/script.js
+++
@@ -1,6 +0,0 @@
-let burgerMenu = document.querySelector('.burger_menu');
-let menu = document.querySelector('.menu')
-
-burgerMenu.addEventListener('click', function(){
-    menu.classList.toggle('active');
-})
